1f878179ad3785b4c5b2653669a78fdcd2f00cdb
tltwyc <Tallestwyc@outlook.com>
2023-09-05 12:34:55 +0800

    myfind

---
+++ README.md
@@ -0,0 +1,3 @@
+# 短学期课程：Rust 开发实训
+
+- Assignment 1: Myfind
--- hello.rs
+++ hello.rs
@@ -1,3 +1,6 @@
 fn main(){
-	println!("Hello, world!");
+	let some_u8_val = Some(0u8);
+	if Some(3) == some_u8_val{
+		println!("three");
+	}
 }
